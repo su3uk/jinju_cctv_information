990fcfb147af622e146024bfc85c88e48d3d721e
JuHyeonuk <susuk0243@naver.com>
2025-06-02 02:53:45 +0000

    진주 cctv 정보 업로드

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+from streamlit_folium import folium_static
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("진주시 CCTV 현황")
+
+df = pd.read_csv("jinju_cctv_20250513.csv", encoding='euc-kr')
+
+st.dataframe(df, height=200)
+
+df[["lat","lon"]] = df[["위도","경도"]]
+
+m = folium.Map(location=[35.1799817, 128.1076213], zoom_start=13)
+
+marker_cluster = MarkerCluster().add_to(m)
+
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row["lat"], row["lon"]],
+        popup=row["설치장소"],
+    ).add_to(marker_cluster)
+
+folium_static(m)
